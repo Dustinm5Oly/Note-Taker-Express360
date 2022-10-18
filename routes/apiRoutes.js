6555b355be65b0c08e608c1b5971fdcd7a05e5d5
Dustinm5Oly <111804479+Dustinm5Oly@users.noreply.github.com>
2022-10-18 09:55:02 -0700

    fixed delete action on apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,30 +1,46 @@
-var dataBase = require(`../db/db.json`);
+// var dataBase = require(`../db/db.json`);
 const fs = require(`fs`);
 
 module.exports = function (app) {
     app.get("/api/notes", function (req, res) {
-        res.json(dataBase);
+        let db = JSON.parse(fs.readFileSync("./db/db.json"))
+        res.json(db);
+        
     });
 
     app.post("/api/notes", function(req, res){
-        dataBase.push(req.body);
-        dataBase.forEach((obj, i) => {
+        let db = JSON.parse(fs.readFileSync("./db/db.json"))
+        db.push(req.body);
+        
+        db.forEach((obj, i) => {
             obj.id = i + 1;
         });
-        fs.writeFile("./db/db.json", JSON.stringify(dataBase), function (){
-            res.json(dataBase);
+        fs.writeFile("./db/db.json", JSON.stringify(db), function (){
+            res.json(db);
         });
     });
 
-    app.delete(`/api/notes/:id`, (req,res) =>{
-        for (let i = 0; dataBase.length; i++) {
-            if(dataBase[i].id == req.params.id){
-                dataBase.splice(i, 1);
-            };
+ 
+    app.delete("/api/notes/:id", (req, res) => {
+        let db = JSON.parse(fs.readFileSync("./db/db.json"))
+        let deleteNote = db.filter(item => item.id != req.params.id);
+        fs.writeFileSync(`./db/db.json`, JSON.stringify(deleteNote));
+        res.json(deleteNote);
+   
+    });
+};
+
+// FOR FUTURE DEVELOPMENT: MAKE THESE WORK!!!!
+
+   // app.delete(`/api/notes/:id`, (req,res) =>{
+    //     for (let i = 0; dataBase.length; i++) {
+    //         if(dataBase[i].id == req.params.id){
+    //             dataBase.splice(i, 1);
+    //         };
             
-        };
-        res.json("deleted")
-    })
+    //     };
+    //     res.json("deleted")
+    // })
     // app.delete(`/api/notes/:id`, function (req, res){
     //     let id = parseInt(req.params.id);
     //     fs.readFile("db/db.json", `utf8`, (err, data) =>{
@@ -46,12 +62,3 @@ module.exports = function (app) {
     //         res.end(JSON.stringify(data));
     //     })
     // })
-    // app.delete("./api/notes/:id", (req, res) => {
-    //     let db = JSON.parse(fs.readFileSync("../db/db.json"))
-    //     //fs.readFile("../db/db.json")
-    //     let deleteNote = db.filter(item => item.id !== req.params.id);
-    //     fs.writeFileSync(`..//db.json`, JSON.stringify(deleteNote));
-    //     res.json(deleteNote);
-   
-    // });
-};
